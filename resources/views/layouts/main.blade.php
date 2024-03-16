faabda55d667c5aaa978e7c8338984749361dd25
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-16 20:05:20 +0100

    add and fixed structure

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="./src/img/nero.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>MODEL CONTROLLER | @yield('title')</title>
+
+  {{--ASSETS VITE--}}
+  @vite('resources/js/app.js')
+
+  {{--CDNS--}}
+  @yield('cdns')
+
+</head>
+
+<body>
+  {{--HEADER--}}
+   @include('includes.header')
+
+  {{--MAIN--}}
+  <main class="container">
+    @yield('main-content')
+  </main>
+  {{--FOOTER--}}
+  <footer></footer>
+
+
+  {{--SCRIPTS--}}
+  @yield('scripts')
+
+
+</body>
+
+</html>
