755f69a549c1157aba9a935fb1f4b4b6b4454cec
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-16 23:23:58 +0100

    fixed forelse

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,16 @@
 
 <section>
     <div class="row">
-        @forelse($movie as $movies)
+        @forelse($movies as $movie)
             <div>
               <h3>{{$movie->title}}</h3>
               <p>{{$movie->original_title}}</p>
               <p>{{$movie->date}}</p>
               <p>{{$movie->vote}}</p>
               <p>{{$movie->nationality}}</p>
+              
             </div>
         @empty
+        @endforelse
     </div>
 </section>
